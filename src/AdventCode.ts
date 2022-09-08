da68b284239adb0742d7dd6d9c7d3222dfb11233
dhirendra <dhiren.thakulla@gmail.com>
2022-09-08 14:50:40 +0545

    day task 2nd

--- src/AdventCode.ts
+++ src/AdventCode.ts
@@ -9,7 +9,10 @@ export class AdventCode {
             this.dataArray.push(parseInt(data[key]));
         });
        let count= AdventCode.higherSpeedIncreaseCount(this.dataArray);
+       let windowCount= AdventCode.checkWindowsIncreaseCount(this.dataArray);
+       
        console.log("count ==",count)
+       console.log("windowCount ==",windowCount)
     }
     public static higherSpeedIncreaseCount(arrayData:Array<number>): number{
         let count = 0;
@@ -22,6 +25,23 @@ export class AdventCode {
 
         return count;
     }
+
+    public static checkWindowsIncreaseCount(arrayData:Array<number>): number{
+        let count = 0
+        arrayData.forEach((ele,index) =>{
+            if(arrayData[index+3]){
+                let windowFirst = ele + arrayData[index+1] + arrayData[index+2];
+                let windowSecond = arrayData[index+1] + arrayData[index+2] + arrayData[index+3];
+                if(windowFirst < windowSecond){
+                    count++;
+                }
+            }
+            
+        })
+
+        return count;
+    }
+    
 
 
 }
